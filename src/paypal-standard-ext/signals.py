9dcc39d9b11c4f47564c28757b093827f7538b28
Arvid Paeglit <arvi3d@gmail.com>
2010-11-27 14:04:40 +0200

    changed package's name

--- src/paypal-standard-ext/signals.py
+++
@@ -1,5 +0,0 @@
-### -*- coding: utf-8 -*- ####################################################
-""" subscription signals """
-from django.dispatch import Signal
-
-paid = Signal(providing_args=["user", "object", "receipt"])
